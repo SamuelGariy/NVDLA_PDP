ae70a47345eebe87c2400bbd824834e0753a0e24
SamuelGariy <sgariy@princeton.edu>
2023-02-15 10:56:20 -0500

    10.56

--- src/pdp_instrs.cc
+++ src/pdp_instrs.cc
@@ -46,8 +46,8 @@ namespace ilang
         auto consumer = m.state(NVDLA_PDP_S_CONSUMER);
         auto pdp_state = m.state("pdp_state");
         auto stride = m.state("pdp_stride");
-       // auto inputarr[NVDLA_PDP_D_DATA_CUBE_IN_HEIGHT][NVDLA_PDP_D_DATA_CUBE_IN_WIDTH];
-        //auto outputarr[NVDLA_PDP_D_DATA_CUBE_OUT_HEIGHT][NVDLA_PDP_D_DATA_CUBE_OUT_HEIGHT] auto counter = BvConst(0, 5);
+        // auto inputarr[NVDLA_PDP_D_DATA_CUBE_IN_HEIGHT][NVDLA_PDP_D_DATA_CUBE_IN_WIDTH];
+        // auto outputarr[NVDLA_PDP_D_DATA_CUBE_OUT_HEIGHT][NVDLA_PDP_D_DATA_CUBE_OUT_HEIGHT] auto counter = BvConst(0, 5);
         // auto input = m.input("pdp_input")
 
         // Initialize both the PRODUCER and CONSUMER states to be 0
@@ -91,18 +91,18 @@ namespace ilang
 
             // set input height and width
             instr.SetUpdate(m.state(NVDLA_PDP_D_DATA_CUBE_IN_HEIGHT), BvConst(4, NVDLA_PDP_D_DATA_CUBE_IN_HEIGHT_WIDTH));
-                instr.SetUpdate(m.state(NVDLA_PDP_D_DATA_CUBE_IN_WIDTH), BvConst(4, NVDLA_PDP_D_DATA_CUBE_IN_WIDTH_WIDTH));
+            instr.SetUpdate(m.state(NVDLA_PDP_D_DATA_CUBE_IN_WIDTH), BvConst(4, NVDLA_PDP_D_DATA_CUBE_IN_WIDTH_WIDTH));
 
-                //  set kernel height and width
-                instr.SetUpdate(m.state(NVDLA_PDP_D_RECIP_KERNEL_HEIGHT), BvConst(2, NVDLA_PDP_D_RECIP_KERNEL_HEIGHT_WIDTH));
-                    instr.SetUpdate(m.state(NVDLA_PDP_D_RECIP_KERNEL_WIDTH), BvConst(2, NVDLA_PDP_D_RECIP_KERNEL_WIDTH_WIDTH));
-                // set stride
-                //instr.SetUpdate(pdp_stride, BvConst(2, 2));
-                //pdp_stride = BvConst(2,2);
+            //  set kernel height and width
+            instr.SetUpdate(m.state(NVDLA_PDP_D_RECIP_KERNEL_HEIGHT), BvConst(2, NVDLA_PDP_D_RECIP_KERNEL_HEIGHT_WIDTH));
+            instr.SetUpdate(m.state(NVDLA_PDP_D_RECIP_KERNEL_WIDTH), BvConst(2, NVDLA_PDP_D_RECIP_KERNEL_WIDTH_WIDTH));
+            // set stride
+            // instr.SetUpdate(pdp_stride, BvConst(2, 2));
+            // pdp_stride = BvConst(2,2);
 
-                // set output
-                instr.SetUpdate(m.state(NVDLA_PDP_D_DATA_CUBE_OUT_HEIGHT), BvConst(2, NVDLA_PDP_D_DATA_CUBE_OUT_HEIGHT_WIDTH));
-                    instr.SetUpdate(m.state(NVDLA_PDP_D_DATA_CUBE_OUT_WIDTH), BvConst(2, NVDLA_PDP_D_DATA_CUBE_OUT_WIDTH_WIDTH));
+            // set output
+            instr.SetUpdate(m.state(NVDLA_PDP_D_DATA_CUBE_OUT_HEIGHT), BvConst(2, NVDLA_PDP_D_DATA_CUBE_OUT_HEIGHT_WIDTH));
+            instr.SetUpdate(m.state(NVDLA_PDP_D_DATA_CUBE_OUT_WIDTH), BvConst(2, NVDLA_PDP_D_DATA_CUBE_OUT_WIDTH_WIDTH));
 
             //             for (auto i = 0; i < NVDLA_PDP_D_DATA_CUBE_IN_HEIGHT; i++)
             // {
@@ -113,62 +113,62 @@ namespace ilang
             //     //     counter = counter + BvConst(1, 5)
             //     // }
             // }
+
+            instr.SetUpdate(pdp_state, MAXPOOL);
         }
 
-        instr.SetUpdate(pdp_state, MAXPOOL);
-    }
+        { // PDP Set Start Group 0 (addr:008)
+            auto instr = m.NewInstr("max_pool");
+            instr.SetDecode(pdp_state == MAXPOOL);
 
-    { // PDP Set Start Group 0 (addr:008)
-        auto instr = m.NewInstr("max_pool");
-        instr.SetDecode(pdp_state == MAXPOOL);
-
-        auto mem_ptr = MemConst(0, {}, 4, 32).get();
-        for (auto output_i = 0; output_i < NVDLA_PDP_D_DATA_CUBE_OUT_HEIGHT; output_i++)
-        {
-            for (int output_j = 0; output_j < outputHeight_Width; output_j++)
+            auto mem_ptr = MemConst(0, {}, 4, 32).get();
+            for (auto output_i = 0; output_i < NVDLA_PDP_D_DATA_CUBE_OUT_HEIGHT; output_i++)
             {
-                for (int kernel_i = 0; kernel_i < 2; kernel_i++)
+                for (int output_j = 0; output_j < outputHeight_Width; output_j++)
                 {
-                    for (int kernel_j = 0; kernel_j < 2; kernel_j++)
+                    for (int kernel_i = 0; kernel_i < 2; kernel_i++)
                     {
-                        // load from memory and read
-                        auto i = output_j * 2 + kernel_i;
-                        auto j = output_i * 2 + kernel_j;
-                        auto curr = m.input("pdp_input", (std::to_string(i)) + "_" + (std::to_string(j)));
-                        auto mem_addr = i + j;
-                        auto max = Load(ExprRef(mem_ptr), BvConst(mem_addr, 4));
+                        for (int kernel_j = 0; kernel_j < 2; kernel_j++)
+                        {
+                            // load from memory and read
+                            auto i = output_j * 2 + kernel_i;
+                            auto j = output_i * 2 + kernel_j;
+                            auto curr = m.input("pdp_input", (std::to_string(i)) + "_" + (std::to_string(j)));
+                            auto mem_addr = i + j;
+                            auto max = Load(ExprRef(mem_ptr), BvConst(mem_addr, 4));
 
-                        instr.SetUpdate(max, Ite(curr > max, curr, max));
-                        // outputarr[output_i][output_j]
+                            instr.SetUpdate(max, Ite(curr > max, curr, max));
+                            // outputarr[output_i][output_j]
 
-                        auto new_mem = ExprRef(mem_ptr).Store(BvConst(mem_addr, 4), max);
-                        mem_ptr = new_mem.get();
+                            auto new_mem = ExprRef(mem_ptr).Store(BvConst(mem_addr, 4), max);
+                            mem_ptr = new_mem.get();
+                        }
                     }
+
+                    // instr.SetUpdate(inputarr[i][j],Extract(m.input("pdp_input"+ (std::to_string(counter))), 31, 0 ));
+                    // instr.SetUpdate(m.state("pdp_input"+ (std::to_string(counter))), );
+                    // counter = counter + BvConst(1,5)
                 }
-
-                // instr.SetUpdate(inputarr[i][j],Extract(m.input("pdp_input"+ (std::to_string(counter))), 31, 0 ));
-                // instr.SetUpdate(m.state("pdp_input"+ (std::to_string(counter))), );
-                // counter = counter + BvConst(1,5)
             }
+            instr.SetUpdate(pdp_state, STORE);
         }
-        instr.SetUpdate(pdp_state, STORE);
-    }
 
-    { // PDP Set Start Group 0 (addr:008)
-        auto instr = m.NewInstr("store");
-        instr.SetDecode(pdp_state == STORE);
+        { // PDP Set Start Group 0 (addr:008)
+            auto instr = m.NewInstr("store");
+            instr.SetDecode(pdp_state == STORE);
 
-    //     auto counter = 0 for (auto i = 0; i < NVDLA_PDP_D_DATA_CUBE_OUT_HEIGHT; i++)
-    //     {
-    //         for (auto j = 0; j < NVDLA_PDP_D_DATA_CUBE_OUT_WIDTH; j++)
-    //         {
-    //             auto curr = BvConst(outputarr[i][j], 32)
-    //                             instr.SetUpdate(m.state("pdp_output" + (std::to_string(counter))), curr);
-    //             counter = counter + BvConst(1, 5)
-    //         }
-    //     }
-    // }
-    instr.SetUpdate(pdp_state, START);
+            //     auto counter = 0 for (auto i = 0; i < NVDLA_PDP_D_DATA_CUBE_OUT_HEIGHT; i++)
+            //     {
+            //         for (auto j = 0; j < NVDLA_PDP_D_DATA_CUBE_OUT_WIDTH; j++)
+            //         {
+            //             auto curr = BvConst(outputarr[i][j], 32)
+            //                             instr.SetUpdate(m.state("pdp_output" + (std::to_string(counter))), curr);
+            //             counter = counter + BvConst(1, 5)
+            //         }
+            //     }
+            // }
+            instr.SetUpdate(pdp_state, START);
+        }
     }
 
 } // namespace ilang
