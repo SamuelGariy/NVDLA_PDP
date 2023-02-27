49d5ba503d27ad320e89594a137f36499f87c396
SamuelGariy <sgariy@princeton.edu>
2023-02-27 16:00:45 -0500

    16.00

--- src/pdp_instrs.cc
+++ src/pdp_instrs.cc
@@ -414,7 +414,9 @@ namespace ilang
             instr.SetUpdate(m.state("pdp_state"), MAXPOOL);
             pdp_state = MAXPOOL;
 
-            auto output_height = m.state(NVDLA_PDP_D_DATA_CUBE_OUT_HEIGHT).val();
+            auto input_height = BvVal(m.state(NVDLA_PDP_D_DATA_CUBE_IN_HEIGHT));
+            
+            auto output_height = m.state(NVDLA_PDP_D_DATA_CUBE_OUT_HEIGHT);
             auto output_width = m.state(NVDLA_PDP_D_DATA_CUBE_OUT_WIDTH);
             auto kernel_height = m.state(NVDLA_PDP_D_KERNEL_HEIGHT);
             auto kernel_width = m.state(NVDLA_PDP_D_KERNEL_WIDTH);
