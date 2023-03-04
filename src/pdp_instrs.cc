4009c20cfa867d83bfa09cc3d3d5abac22ad2c56
SamuelGariy <sgariy@princeton.edu>
2023-03-03 21:29:55 -0500

    21.29

--- src/pdp_instrs.cc
+++ src/pdp_instrs.cc
@@ -454,7 +454,7 @@ namespace ilang
             auto split_stage = m.state("pdp_pooling_stage_split_width");
 
             // update output width in use depending on mode
-            output_width = Ite(mode == PDP_OFF_FLYING_NO_SPLIT, output_width_first, Ite(mode == PDP_OFF_FLYING_NO_SPLIT, Ite(split_stage == SPLIT_STAGE_1, output_width_first, Ite(split_stage == SPLIT_STAGE_2, output_width_mid, output_width_last))) output_width);
+            output_width = Ite(mode == PDP_OFF_FLYING_NO_SPLIT, output_width_first, Ite(mode == PDP_OFF_FLYING_SPLIT, Ite(split_stage == SPLIT_STAGE_1, output_width_first, Ite(split_stage == SPLIT_STAGE_2, output_width_mid, output_width_last))), output_width);
 
             // share line buffer
             auto share_buffer_ptr = MemConst(SHRT_MIN, {}, PDP_SHARE_LINE_ADDR_WIDTH, PDP_INT_16_WIDTH).get();
@@ -465,8 +465,7 @@ namespace ilang
             for (auto output_j = 0; output_j < PDP_OUTPUT_MAX; output_j++)
             {
                 // skip output_update when operation is over
-                bool skip_output = output_j < output_width ? false : true;
-                auto skip_output_bv = BoolConst(skip_output);
+                auto skip_output_bv = Ite(BvConst(output_j, NVDLA_PDP_D_DATA_CUBE_OUT_WIDTH_WIDTH) < output_width, BoolConst(false), BoolConst(true));
                 int actual_output_j = output_j < output_width ? output_j : output_width - 1;
                 auto max = BvConst(SHRT_MIN, PDP_INT_16_WIDTH);
 
