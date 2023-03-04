5eb40990ca842fb55f3eb7177826839c92e962a3
SamuelGariy <sgariy@princeton.edu>
2023-03-03 21:49:25 -0500

    21.49

--- src/pdp_instrs.cc
+++ src/pdp_instrs.cc
@@ -481,7 +481,7 @@ namespace ilang
 
                     max = Ite(curr > max, curr, max);
                 }
-                auto curr_max = Load(m.state("pdp_share_line_buffer", BvConst(output_j, PDP_SHARE_LINE_ADDR_WIDTH)));
+                auto curr_max = Load(m.state("pdp_share_line_buffer"), BvConst(output_j, PDP_SHARE_LINE_ADDR_WIDTH));
 
                 skip_output_bv = Ite(max > curr_max, skip_output_bv, BoolConst(true));
 
