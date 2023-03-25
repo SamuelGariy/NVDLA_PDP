fd9db2f2503735f71c602e18a78e3aca66380b8c
SamuelGariy <47086904+SamuelGariy@users.noreply.github.com>
2023-03-25 17:10:12 -0500

    17.09

--- batch_input_generator_script.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-# define the number of times to run the script
-num_runs = 5
-
-# loop through and run the script multiple times
-for i in range(num_runs):
-    subprocess.run(['python', 'path/to/your/script.py'])
