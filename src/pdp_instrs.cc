83bf3d9c3c131f3524245bc04e32fb861ea818be
SamuelGariy <sgariy@princeton.edu>
2023-04-11 16:46:35 -0400

    16.46

--- src/pdp_instrs.cc
+++ src/pdp_instrs.cc
@@ -483,7 +483,8 @@ namespace ilang
             auto kernel_height = m.state(GetVarName("group0_", NVDLA_PDP_D_KERNEL_HEIGHT));
             auto kernel_width = m.state(GetVarName("group0_", NVDLA_PDP_D_KERNEL_WIDTH));
             auto data_format = m.state(GetVarName("group0_", NVDLA_PDP_D_DATA_FORMAT));
-            auto kernel_size = ZExt(kernel_height,PDP_INT_16_WIDTH) * ZExt(kernel_width,PDP_INT_16_WIDTH);
+           // auto kernel_size = ZExt(kernel_height,PDP_INT_16_WIDTH) * ZExt(kernel_width,PDP_INT_16_WIDTH);
+            auto kernel_size = ZExt(kernel_height,32) * ZExt(kernel_width,32);
 
             auto sum = BvConst(0, 32);// 32 bits to keep accuracy
 
@@ -498,9 +499,9 @@ namespace ilang
             }
             
            //auto neg_mean = Ite(kernel_size > BvConst(0, PDP_INT_16_WIDTH), pos_to_neg((neg_to_pos(sum) / ZExt(kernel_size,32))), BvConst(0, 32));
-        auto pos_mean = Ite(kernel_size > BvConst(0, PDP_INT_16_WIDTH), (sum * (1/ZExt(kernel_size,32))), BvConst(0, 32));
+      //  auto pos_mean = Ite(kernel_size > BvConst(0, PDP_INT_16_WIDTH), (sum / ZExt(kernel_size,32)), BvConst(0, 32));
         auto neg_mean = sum;
-       // auto pos_mean = sum;
+        auto pos_mean = Ite(kernel_size > BvConst(0, PDP_INT_16_WIDTH), (sum / kernel_size), BvConst(0, 32));
 
 
            auto mean = Ite(SelectBit(sum,31) == 1, Extract(neg_mean,PDP_INT_16_WIDTH-1,0),Extract(pos_mean,PDP_INT_16_WIDTH-1,0));
