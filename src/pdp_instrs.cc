12f3d30658cd357f2bb6c0faa65431e6e84a2546
SamuelGariy <sgariy@princeton.edu>
2023-04-11 16:50:52 -0400

    16.50

--- src/pdp_instrs.cc
+++ src/pdp_instrs.cc
@@ -501,7 +501,7 @@ namespace ilang
            //auto neg_mean = Ite(kernel_size > BvConst(0, PDP_INT_16_WIDTH), pos_to_neg((neg_to_pos(sum) / ZExt(kernel_size,32))), BvConst(0, 32));
       //  auto pos_mean = Ite(kernel_size > BvConst(0, PDP_INT_16_WIDTH), (sum / ZExt(kernel_size,32)), BvConst(0, 32));
         auto neg_mean = sum;
-        auto pos_mean = Ite(kernel_size > BvConst(0, PDP_INT_16_WIDTH), (sum / kernel_size_32), BvConst(0, 32));
+        auto pos_mean = Ite(kernel_size > BvConst(0, PDP_INT_16_WIDTH), (sum * (BvConst(1,32)/kernel_size_32)), BvConst(0, 32));
 
 
            auto mean = Ite(SelectBit(sum,31) == 1, Extract(neg_mean,PDP_INT_16_WIDTH-1,0),Extract(pos_mean,PDP_INT_16_WIDTH-1,0));
